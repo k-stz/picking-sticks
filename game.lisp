49ca9ff35278594a6c5725a1e96324a178c45520
k-stz@triton <keistzen@gmail.com>
2015-05-24 19:15:04 +0200

    gl programs added

--- game.lisp
+++ game.lisp
@@ -22,14 +22,8 @@
 ;;;
 ;;;   (make-instance 'sdl2.kit.test:test-window)
 ;;;
-;;; You can make multiple windows if you want.  Note that, despite not
-;;; assigning the value, THIS IS NOT COLLECTED.  A reference is kept
-;;; to all windows:
-;;;
-;;;   (all-windows)
-;;;
 ;;; After you close a window, it will be collected at some point.
-;;;
+
 ;;; You should NOT call any protocol functions on a window, except the
 ;;; following:
 ;;;
@@ -52,58 +46,85 @@
 ;;; Note this is an :AFTER method.  You should either use :AFTER, or
 ;;; you must (CALL-NEXT-METHOD).
 
+
+(defvar *triangle-data*
+  (cffi:foreign-alloc :float
+		      :initial-contents '(-0.5 -0.5 0.0 1.0
+					  0.0 0.5 0.0 1.0
+					  0.5 -0.5 0.0 1.0)))
+
+
+(defparameter *vertex-shader* "
+// simple passthrough shader
+#version 330
+layout(location = 0) in vec4 position;
+void main () {
+    gl_Position = position;
+}")
+
+(defparameter *fragment-shader* "
+#version 330
+// simple fragment shader assigning the same outputColor on all fragments
+out vec4 outputColor;
+void main() {
+outputColor = vec4(0.0f, 1.0f, 1.0f, 1.0f);
+}"
+)
+
+
+(defun initialize-program ())
+
 (defmethod initialize-instance :after ((w test-window) &key &allow-other-keys)
   ;; GL setup can go here; your GL context is automatically active,
   ;; and this is done in the main thread.
-
 
   ;; if you (setf (idle-render window) t) it'll call RENDER as fast as
   ;; possible when not processing other events; suitable for games
   (setf (idle-render w) t)
   (gl:clear-color 0 0 1 1)
   (gl:clear :color-buffer-bit)
-  (gl:viewport 0 0 800 600))
+  (gl:viewport 0 0 800 600)
+
+  ;; setup buffer-data
+  (let ((position-buffer-object (first (gl:gen-buffers 1))))
+    (gl:bind-buffer :array-buffer position-buffer-object)
+    (%gl:buffer-data :array-buffer 12 *triangle-data* :static-draw))
+  (initialize-program)
+  (gl:enable-vertex-attrib-array 0))
 
 
-(defparameter *frames-per-second* 60 "Used by FRAMELIMIT to set the FPS")
-
-(defun framelimit (window)
-  "SDL2:DELAY to get desired FPS set in the *frames-per-second* variable."
+(defun framelimit (window &optional (fps 60))
+  "SDL2:DELAY to get desired FPS."
   (with-slots (one-frame-time) window
     (let ((elapsed-time (- (get-internal-real-time) one-frame-time))
-	  (time-per-frame (/ 1000.0 *frames-per-second*)))
+	  (time-per-frame (/ 1000.0 fps)))
       (when (< elapsed-time time-per-frame)
 	(sdl2:delay (floor (- time-per-frame elapsed-time))))
       (setf one-frame-time (get-internal-real-time)))))
 
+
+(defun display-fps (window)
+  (with-slots (start-time frames) window
+    (incf frames)
+    (let* ((current-time (get-internal-real-time))
+	   (seconds (/ (- current-time start-time) internal-time-units-per-second)))
+      (when (> seconds 5)
+	(format t "FPS: ~A~%" (float (/ frames seconds)))
+	(setf frames 0)
+	(setf start-time (get-internal-real-time))))))
+
+
+
+
+
 (defmethod render ((window test-window))
-    ;; Your GL context is automatically active.  FLUSH and
-    ;; SDL2:GL-SWAP-WINDOW are done implicitly by GL-WINDOW  (!!)
-    ;; after RENDER.
-    (with-slots (rotation) window
-      (gl:load-identity)
-      (gl:rotate rotation 0 0 1)
-      (gl:clear-color 0.0 0.0 1.0 1.0)
-      (gl:clear :color-buffer)
-      (gl:begin :triangles)
-      (gl:color 1.0 0.0 0.0)
-      (gl:vertex 0.0 1.0)
-      (gl:vertex -1.0 -1.0)
-      (gl:vertex 1.0 -1.0)
-      (gl:end))
-
+  ;; Your GL context is automatically active.  FLUSH and
+  ;; SDL2:GL-SWAP-WINDOW are done implicitly by GL-WINDOW  (!!)
+  ;; after RENDER.
   
-    ;;display fps
-    (with-slots (start-time frames) window
-      (incf frames)
-      (let* ((current-time (get-internal-real-time))
-	     (seconds (/ (- current-time start-time) internal-time-units-per-second)))
-	(when (> seconds 5)
-	  (format t "FPS: ~A~%" (float (/ frames seconds)))
-	  (setf frames 0)
-	  (setf start-time (get-internal-real-time)))))
 
-    (framelimit window))
+  (display-fps window)
+  (framelimit window 60))
 
 (defmethod close-window ((window test-window))
   (format t "Bye!~%")
