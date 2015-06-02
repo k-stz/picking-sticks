35f6038c516a6472c8736d042608ca4ad0b358e6
k-stz@triton <keistzen@gmail.com>
2015-06-02 20:31:20 +0200

    cleaned up code, next be cepl tests

--- game.lisp
+++ game.lisp
@@ -41,24 +41,14 @@
 (defclass test-window (kit.sdl2:gl-window)
   ((start-time :initform (get-internal-real-time))
    (one-frame-time :initform (get-internal-real-time))
-   (frames :initform 0)
-   (rotation :initform 0.0)))
-
-;;; All of these methods are OPTIONAL.  However, without a render
-;;; method, your window will not look like much!
+   (frames :initform 0)))
 
 
-;;; Note this is an :AFTER method.  You should either use :AFTER, or
-;;; you must (CALL-NEXT-METHOD).
 
 
-(defvar *triangle-data*
-  (cffi:foreign-alloc :float
-		      :initial-contents '(-0.5 -0.5 0.0 1.0
-					  0.0 0.5 0.0 1.0
-					  0.5 -0.5 0.0 1.0)))
+;;Data:--------------------------------------------------------------------------
 
-(defvar *cube-data*
+(defvar *cube-positions*
   (cffi:foreign-alloc
    :float
    :initial-contents
@@ -69,80 +59,64 @@
      0.5 0.5 -0.5			;4
      0.5 -0.5 -0.5			;5
      -0.5 -0.5 -0.5			;6
-     -0.5 0.5 -0.5                      ;7
-     ;;color data
-     0.1 0.1 0.1			;8
-     0.2 0.2 0.2			;9
-     0.3 0.3 0.3			;10
-     0.4 0.4 0.4			;11
-     0.5 0.5 0.5			;12
-     0.6 0.6 0.6 			;13
-     0.7 0.7 0.7			;14
-     0.8 0.8 0.8)))	         	;15
+     -0.5 0.5 -0.5)))			;7
 
 (defvar *cube-colors*
   (cffi:foreign-alloc
    :float
    :initial-contents
-   '(0.2 0.2 0.2
-     0.3 0.3 0.3
-     0.4 0.4 0.4
-     0.5 0.5 0.5
-     0.6 0.6 0.6
-     0.7 0.7 0.7
-     0.8 0.8 0.8
-     0.9 0.9 0.9)))
+   '(0.2 0.2 0.2			;0
+     0.3 0.3 0.3			;1
+     0.4 0.4 0.4			;2
+     0.5 0.5 0.5			;3
+     0.6 0.6 0.6			;4
+     0.7 0.7 0.7			;5
+     0.8 0.8 0.8			;6
+     0.9 0.9 0.9)))			;7
 
 (defvar *cube-indices*
   (cffi:foreign-alloc
    :unsigned-short
    :initial-contents
-   '(0 1 2
+   '(;; front
+     0 1 2
      2 3 0
-
+     ;; right-side     
      4 5 1  
      1 0 4
-
+     ;; back
      7 6 5
      5 4 7
-
+     ;; left-side
      3 2 6
      6 7 3
-
+     ;; top
      4 0 3
      3 7 4
-
+     ;; bottom
      1 5 6
      6 2 1)))
 
 
 ;;Shader------------------------------------------------------------------------
 
-
-;; the returns dictioary with the program can be used like so:
-;; (1) get the program directly (find-program <compiled-dictionary> <program-name>
-;; (2) or just use it directily (use-program <compiled-dictionary> <program-name>
+;; the returned dictionary with the programs can be used like so:
+;; (1) get the program directly (find-program <compiled-dictionary> <program-name>)
+;; (2) or just use it directly (use-program <compiled-dictionary> <program-name>)
 ;;     also (use-program 0) works
 (defun load-shaders ()
   (defdict shaders (:shader-path
-		    ;; TODO: check out tradewarz' GET-PATH
 		    (merge-pathnames
-		     #p "shaders/" (asdf/system:system-source-directory :picking-sticks)))
-    (shader pass-through-v :vertex-shader (:file "pass-through.vert"))
-    (shader one-transform-v :vertex-shader (:file "one-transform.vert"))
+		     #p "examples/shaders/" (asdf/system:system-source-directory :sdl2kit-examples)))
+    ;; instead of (:file <path>) you may directly provide the shader as a string containing the
+    ;; source code
     (shader matrix-perspective-v :vertex-shader (:file "transform-and-project.vert"))
-    (shader same-color-f :fragment-shader (:file "same-color.frag"))
     (shader color-pass-through-f :fragment-shader (:file "color-pass-through.frag"))
-    (program :basic (:color) ;; <- UNIFORMS!
-	     (:vertex-shader pass-through-v)
-	     (:fragment-shader same-color-f))
-    (program :basic-transform (:model-to-clip :color)
-	     (:vertex-shader one-transform-v)
-	     (:fragment-shader same-color-f))
-    (program :basic-projection (:model-to-clip :perspective-matrix)
+    ;; here we compose the shaders into programs, in this case just one ":basic-projection"
+    (program :basic-projection (:model-to-clip :perspective-matrix) ;<- UNIFORMS!
 	     (:vertex-shader matrix-perspective-v)
 	     (:fragment-shader color-pass-through-f)))
-  ;; funciton may only run when a gl-context exists, as it's documentation
+  ;; function may only run when a gl-context exists, as its documentation
   ;; mentions
   (compile-shader-dictionary 'shaders))
 
@@ -150,7 +124,6 @@
 
 (defun initialize-program ()
   (setf *programs-dict* (load-shaders)))
-
 
 ;; to be understood while reading the LOAD-SHADER function
 ;; example: (uniform :vec :<name-of-uniform> <new-value>)
@@ -166,17 +139,10 @@
     (uniform-matrix *programs-dict* key 4 value NIL)))
 
 
-;; TODO: why specialize on SHADE
-
-
-;;..............................................................................
-
-
-(defun initialize-gl-vao ()
-  ())
-
+;;VAO setup.....................................................................
 
 (defvar *vao* 0)
+
 (defun initialize-vao ()
   (let ((vao (first (gl:gen-vertex-arrays 1)))
 	(vbo (first (gl:gen-buffers 1)))
@@ -185,64 +151,42 @@
     ;;VBO
     (gl:bind-buffer :array-buffer vbo)
     ;;VBO - positions
-    (%gl:buffer-data :array-buffer (* 24 4 3 2) *cube-data* :static-draw)
+    ;;to avoid magic numbers (* 24 4 3 2) the gl:gl-array (gl:alloc-gl-array) can be used,
+    ;;which is a struct containing field with a pointer to the forein-memory and a field
+    ;;with the size.  In this case ommited for the sake of a terse example.
+    ;; Layout:
+    ;; 24 number of vertices/colors
+    ;; 4 size of the float data type
+    ;; 3 x,y,z coordinate (positions) or red,green,blue (colors)
+    ;; 2 array contains 24 position vertices and, again, 24 color vertices
+    (%gl:buffer-data :array-buffer (* 24 4 3 2) *cube-positions* :static-draw)
     (%gl:enable-vertex-attrib-array 0)
     (%gl:vertex-attrib-pointer 0 3 :float :false 0 0)
     ;;VBO - colors
+    ;; color sub-data starts in vbo exactly after the position vertices hence
+    ;; the offset (* 24 4 3) and its size is also (* 24 4 3) as every vertices
+    ;; has its own color
     (%gl:buffer-sub-data :array-buffer (* 24 4 3) (* 24 4 3) *cube-colors*)
     (%gl:enable-vertex-attrib-array 1)
     (%gl:vertex-attrib-pointer 1 3 :float :false 0 0)
     
     ;;IBO
     (gl:bind-buffer :element-array-buffer ibo)
+    ;; why (* 36 2)?
+    ;; it takes 2 triangles to draw the side of cube, hence to draw a whole cube:
+    ;; (* 2 6) => 12. Each triangle consists of 3 vertices, hence, (* 3 12) => 36
+    ;; and the index buffer's indices first point to the vertices in the vbo,
+    ;; supplied by *cube-positions*, and then for each position to the corresponding
+    ;; color in the vbo, supplied by *cube-colors*, hence 36 times 2!
     (%gl:buffer-data :element-array-buffer (* 36 2) *cube-indices* :static-draw)
 
-    ;;TODO: do you need to fill the arrays into gl first and _then_ bind them
-    ;;after the vao implicitly?
     (gl:bind-vertex-array 0)
     (setf *vao* vao)))
 
-
-(defvar *position-buffer-object*)
-
-
-(defmethod initialize-instance :after ((w test-window) &key &allow-other-keys)
-  ;; GL setup can go here; your GL context is automatically active,
-  ;; and this is done in the main thread.
-
-  ;; if you (setf (idle-render window) t) it'll call RENDER as fast as
-  ;; possible when not processing other events - suitable for games
-  (setf (idle-render w) t)
-  (gl:clear-color 0 0 1 1)
-  (gl:clear :color-buffer-bit)
-  (gl:viewport 0 0 800 600)
-
-  ;; with culling
-  (gl:enable :cull-face)
-  (gl:cull-face :back)
-  (gl:front-face :cw)
-
-  
-  ;; shader stuff
-  (initialize-program)
-
-  ;; vao
-  (initialize-vao)
-  
-  ;; enable v-sync 0, disable with 0 TODO: test with moving triangle at high velocity
-  ;; if tearing disappears, or if it is an os issue
-  ;; (sdl2:gl-set-swap-interval 1) 
-
-  ;; setup buffer-data
-  (setf *position-buffer-object* (first (gl:gen-buffers 1)))
-  (gl:bind-buffer :array-buffer *position-buffer-object*)
-  (%gl:buffer-data :array-buffer 48 *triangle-data* :static-draw)
-  (initialize-program)
-  (gl:enable-vertex-attrib-array 0))
-
+;;utils-------------------------------------------------------------------------
 
 (defun framelimit (window &optional (fps 60))
-  "SDL2:DELAY to get desired FPS."
+  "Issues SDL2:DELAY's to get desired FPS."
   (with-slots (one-frame-time) window
     (let ((elapsed-time (- (get-internal-real-time) one-frame-time))
 	  (time-per-frame (/ 1000.0 fps)))
@@ -262,7 +206,10 @@
 	(setf start-time (get-internal-real-time))))))
 
 
+;;init code---------------------------------------------------------------------
 
+
+;; TODO build vao
 (defun draw-triangle ()
   (use-program *programs-dict* :basic-transform)
   (uniform :vec :color #(1.0 0.0 0.0 1.0))
@@ -280,24 +227,62 @@
   (gl:use-program 0))
 
 
-(defparameter *rotate-x* -1.0)
+(defvar *position-buffer-object*)
+
+(defmethod initialize-instance :after ((w test-window) &key &allow-other-keys)
+  ;; GL setup can go here; your GL context is automatically active,
+  ;; and this is done in the main thread.
+
+  ;; if you (setf (idle-render window) t) it'll call RENDER as fast as
+  ;; possible when not processing other events - suitable for games
+  (setf (idle-render w) t)
+  (gl:clear-color 0 0 1 1)
+  (gl:clear :color-buffer-bit)
+  (gl:viewport 0 0 800 600)
+
+  ;; with culling
+  (gl:enable :cull-face)
+  (gl:cull-face :back)
+  (gl:front-face :cw)
+
+  (initialize-program)
+  (initialize-vao)
+
+  ;; TODO: clean this up and do the tests
+    ;; enable v-sync 0, disable with 0 TODO: test with moving triangle at high velocity
+  ;; if tearing disappears, or if it is an os issue
+  ;; (sdl2:gl-set-swap-interval 1) 
+
+  ;; setup buffer-data
+  ;; TODO: make vao
+  ;; (setf *position-buffer-object* (first (gl:gen-buffers 1)))
+  ;; (gl:bind-buffer :array-buffer *position-buffer-object*)
+  ;; (%gl:buffer-data :array-buffer 48 *triangle-data* :static-draw)
+  ;; (initialize-program)
+  ;; (gl:enable-vertex-attrib-array 0)
+)
+
+;;Rendering----------------------------------------------------------------------
+
+(defparameter *rotate-x* 1.0)
 (defparameter *rotate-y* 0.0)
 (defparameter *zoom-z* -2.0)
 
 (defun draw-cube ()
   (gl:bind-vertex-array *vao*)
   (use-program *programs-dict* :basic-projection)
+  ;; all the neat transformations take place here
   (uniform :mat :model-to-clip
 	   (vector
 	    (sb-cga:matrix*
 	     (sb-cga:translate (vec3 0.0 0.0 *zoom-z*))
 	     (sb-cga:rotate (vec3 *rotate-x* *rotate-y* 0.0))
 	     (sb-cga:rotate (vec3 0.0 (mod (/ (sdl2:get-ticks) 5000.0) (* 2 3.14159)) 0.0)))))
-
+  ;; projection matrix
   (uniform :mat :perspective-matrix
-	   (vector (perspective-matrix 170.0 1 0.0 1000.0)))  
+	   (vector (perspective-matrix (* pi 1/3) 1/1 0.0 1000.0)))  
 
-  (%gl:draw-elements :triangles  (* 36 2) :unsigned-short 0)
+  (%gl:draw-elements :triangles (* 36 2) :unsigned-short 0)
   (gl:bind-vertex-array 0))
 
 
@@ -307,11 +292,12 @@
   ;; after RENDER.
   (gl:clear :color-buffer)
 
-;  (draw-triangle)
   (draw-cube)
 
   (display-fps window)
   (framelimit window 60))
+
+;;Events------------------------------------------------------------------------
 
 (defmethod close-window ((window test-window))
   (format t "Bye!~%")
@@ -321,24 +307,14 @@
   (call-next-method))
 
 (defmethod mousewheel-event ((window test-window) ts x y)
-  (with-slots (rotation) window
-    (cond ((= y 1) (incf *zoom-z* 0.2))
-	  ((= y -1) (decf *zoom-z* 0.2)))
-;    (render window)
-    ))
+  ;; zoom in/out
+  (cond ((= y 1) (incf *zoom-z* 0.2))
+	((= y -1) (decf *zoom-z* 0.2)))
+  (render window))
 
-(defmethod textinput-event ((window test-window) ts text) ;
-  ;; (format t "You typed: ~S~%" text)
-  ;; (when (string= "Q" (string-upcase text))
-  ;;   (close-window window))
-  )
 
 (defmethod keyboard-event ((window test-window) state ts repeat-p keysym)
   (let ((scancode (sdl2:scancode keysym)))
-    (unless repeat-p
-      (format t "~A ~S~%" state scancode))
-    (when (eq :scancode-space scancode)
-      (print "Space-key pushed"))
     (when (eq :scancode-escape scancode)
       (close-window window))))
 
@@ -352,5 +328,3 @@
     (when (left-mouse-button-clicked-p)
       (incf *rotate-y* (/ xr 100.0))
       (incf *rotate-x* (/ yr 100.0)))))
-
-;; (make-instance 'test-window)
