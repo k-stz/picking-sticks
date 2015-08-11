d72b6cc59af9bb8e070c32bf28f18da4f8225493
k-stz@triton <keistzen@gmail.com>
2015-08-12 00:04:58 +0200

    orient3d notes and implementation

--- collision.lisp
+++ collision.lisp
@@ -19,6 +19,22 @@ The returned value is also the signed are of the triangle*2"
 	     (v c 0) (v c 1) 1.0 0.0
 	     0.0     0.0     0.0 1.0))))
 
+(defun orient3d (A B C D)
+  "Takes 3d points A, B, C and D. If returned value:
+Above the plane means that viewed from D the triangle appears to be CCW.
+> 0.0: D is above the ABC plane (the plane supporting triangle ABC)
+< 0.0: D below or \"D sees a CW triangle\"
+= 0.0: D is coplanar with ABC.
+The returned value is also the signed are of the tetrahedron*6"
+  (declare (simple-array a b c))
+  (macrolet ((v (vec2 subscript)
+	       `(aref ,vec2 ,subscript)))
+    (matrix-determinant
+     (matrix (v a 0) (v a 1) (v a 2) 1.0
+	     (v b 0) (v b 1) (v b 2) 1.0
+	     (v c 0) (v c 1) (v c 2) 1.0
+	     (v d 0) (v d 1) (v d 2) 1.0))))
+
 
 (defun 2x2-det (mat2x2)
   "Calculate determinant of row-major matrix"
